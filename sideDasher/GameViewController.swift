65bbef5ed00b3b5fb23f1103f9c01ad46cf069c4
Jayden Sawyer <jayden.sawyer25@d155.org>
2025-02-26 08:07:46 -0600

    yo

--- sideDasher/GameViewController.swift
+++ sideDasher/GameViewController.swift
@@ -6,40 +6,97 @@
 //
 
 import UIKit
+
 import SpriteKit
+
 import GameplayKit
 
-class GameViewController: UIViewController {
 
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var deathOutlet: UILabel!
+    var play: GameScene!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
+        
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+        
+        
+        
+        
         if let view = self.view as! SKView? {
+            
             // Load the SKScene from 'GameScene.sks'
+            
             if let scene = SKScene(fileNamed: "GameScene") {
+                
                 // Set the scale mode to scale to fit the window
+                
                 scene.scaleMode = .aspectFill
                 
+                
+                
+                // getting a refrance to game scene.
+                
+                play = scene as? GameScene
+                
+                
+                
+                
+                
                 // Present the scene
+                
                 view.presentScene(scene)
+                
             }
+            
+            
             
             view.ignoresSiblingOrder = true
             
+            
+            
             view.showsFPS = true
+            
             view.showsNodeCount = true
+            
         }
+        
     }
-
+    
+    
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        
+        return .landscapeRight
+        
+        
+        
     }
-
+    
+    
+    
     override var prefersStatusBarHidden: Bool {
+        
         return true
+        
+    }
+    
+    @IBAction func jumpButton(_ sender: Any) {
+        
+        
+        
+        play.ball.physicsBody?.velocity.dy = 1000
+        
+        
+        
     }
 }
